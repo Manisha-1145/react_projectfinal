cd52a1e5ad8d151e6ff9a9d4c1aa1c2e0cf4a04d
Manisha-1145 <thapamanisha9849@gmail.com>
2026-02-02 14:13:59 +0545

    review pages

---
+++ project/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReviewPage from './Layout/ReviewsCard';
+
+function App() {
+  return (
+    <div>
+      <ReviewPage />
+    </div>
+  );
+}
+
+export default App;
